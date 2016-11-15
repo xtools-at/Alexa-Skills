73324f9f5bb31bba4bee6843d3df7c9ce37a2f59
Kainzbauer Martin <M.Kainzbauer@bipa.at>
2016-11-15 16:45:43 +0100

    added onStart function

--- src/app.js
+++ src/app.js
@@ -10,7 +10,20 @@ const app = alexia.createApp('Nachtlicht');
  * Register callback to be executed once app is started without any intent.
  * Example invocation: 'Alexa, start <my-app-name>'
  */
-app.onStart(() => '');
+app.onStart(() => {
+	var baseUrl = 'https://s3.eu-central-1.amazonaws.com/assetsalexa/nachtlicht/';
+
+	var audio = `<audio src="${baseUrl + 's01min.mp3'}" />`;
+	audio += `<audio src="${baseUrl + 's16s.mp3'}" />`;
+	audio += `<audio src="${baseUrl + 's08s.mp3'}" />`;
+
+	var ssml = `<speak>${audio}</speak>`;
+
+	return {
+	  ssml: true,
+	  text: ssml
+	};
+});
 
 // Register all intents matching specified pattern
 app.registerIntents('src/modules/**/*-intent.js');
