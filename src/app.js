88416238c502496bdff5c754ccb7a14ea473962d
Kainzbauer Martin <M.Kainzbauer@bipa.at>
2016-11-17 15:11:40 +0100

    added mandatory built-in Intents

--- src/app.js
+++ src/app.js
@@ -17,6 +17,45 @@ app.onStart(() => {
 	};
 });
 
+
+// handle mandatory built-in Intents
+const stopUtterances = [
+  'stop',
+  'halt',
+  'ausschalten'
+];
+const cancelUtterances = [
+  'abbrechen',
+  'cancel',
+  'aufhoeren'
+];
+const helpUtterances = [
+  'hilfe',
+  'starte hilfe',
+  'hilf mir',
+  'was kann ich sagen'
+];
+
+app.builtInIntent('stop', stopUtterances, () => '');
+
+app.builtInIntent('cancel', cancelUtterances, () => '');
+
+app.builtInIntent('help', helpUtterances, () => {
+  var speech = '<speak>';
+  speech += 'Um Euro Wechselkurse abzurufen, frage mich zum Beispiel <s>Was ist der Kurs von Schweizer Franken</s>';
+  speech += 'Um Waehrungen umzurechnen, frage mich <s>Wieviel sind hundert Dollar in Euro</s>';
+  speech += 'Um spezielle Wechselkurse abzurufen, frage mich <s>Was ist der Kurs von Schweizer Franken und Dollar</s>';
+  speech += '<s>Was darf es sein</s>';
+  speech += '</speak>';
+
+  return {
+    ssml: true,
+    text: speech,
+    end: false
+  };
+});
+
+
 // Register all intents matching specified pattern
 app.registerIntents('src/modules/**/*-intent.js');
 
