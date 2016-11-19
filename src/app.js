14550625e8750343b3af805ced686ca8f8072cea
Martin Kainzbauer <m.kainzbauer@xtools.at>
2016-11-19 14:56:07 +0100

    working on bitcoin skill, not yet working

--- src/app.js
+++ src/app.js
@@ -1,5 +1,8 @@
 'use strict';
 
+const axios = require('axios');
+const moment = require('moment');
+
 /**
  * Main Alexa app entry.
  * Global app configuration and all intent registering belongs here
@@ -12,10 +15,65 @@ const app = alexia.createApp('Bitcoin');
  * Register callback to be executed once app is started without any intent.
  * Example invocation: 'Alexa, start <my-app-name>'
  */
-app.onStart(() => {
-  return {
-    //text: 'Frage mich nach dem Bitcoin Kurs, wieviel einer Waehrung in Bitcoin ausmacht oder anders herum'
-  };
+app.onStart((slots, attrs, data, done) => {
+  var apiKey = JSON.stringify(process.env.QUANDL_API_KEY).replace(/"/g, '');
+  var date = moment().subtract(1, 'days').format('YYYY-MM-DD');
+  var url = `https://www.quandl.com/api/v3/datasets/BTCE/USDBTC.json?api_key=${apiKey}&start_date=${date}`;
+
+  console.log('debug custom: ', url);
+
+  axios.get(url).then(result => {
+    console.log('debug custom: ', result.data);
+    if (result.data) {
+      // handle errors
+      if (!result.data.dataset.data[0] || typeof result.data.dataset.data[0] === 'undefined') {
+        done(() => {
+          return {
+            text: 'Ich konnte die Kurse nicht abrufen'
+          };
+        });
+      }
+      // get usd rates as strings
+      var high = '' + result.data.dataset.data[0][1].toFixed(3);
+      var low = '' + result.data.dataset.data[0][2].toFixed(3);
+      var avg = '' + result.data.dataset.data[0][3].toFixed(3);
+      var last = '' + result.data.dataset.data[0][4].toFixed(3);
+      var buy = '' + result.data.dataset.data[0][5].toFixed(3);
+      var sell = '' + result.data.dataset.data[0][6].toFixed(3);
+
+      // replace dots with commas
+      high = high.replace('.', ',');
+      low = low.replace('.', ',');
+      avg = avg.replace('.', ',');
+      last = last.replace('.', ',');
+      buy = buy.replace('.', ',');
+      sell = sell.replace('.', ',');
+
+      // build output speech
+      var speech = `<speak>`;
+      speech += `<s>Der heutige Durschschnittskurs fuer einen Bitcoin steht bei ${avg} U.S. Dollar, der aktuellste Preis ist ${last}</s>`;
+      speech += `<s>Der Kaufpreis steht bei ${buy}, der Verkaufspreis bei ${sell} Dollar</s>`;
+      speech += `<s>Der heutige Hoechststand war ${high}, der Tiefststand ${low} Dollar</s>`;
+      speech += `</speak>`;
+
+      console.log('debug custom: ', speech);
+
+      done(() => {
+        return {
+          ssml: true,
+          text: speech
+        };
+
+      });
+
+    } else {
+      done(() => {
+        return {
+          text: 'Ich konnte die Kurse nicht abrufen'
+        };
+      });
+    }
+  });
 });
 
 // Register all intents matching specified pattern
--- src/modules/bitcoin/bcRate-intent.js
+++ src/modules/bitcoin/bcRate-intent.js
@@ -1,64 +1,88 @@
 'use strict';
 
-const dictionary = require('./dictionary');
 const axios = require('axios');
+const moment = require('moment');
 
 module.exports = app => {
-  // app.customSlot('Currency', Object.keys(dictionary.currencies));
 
-  const eurUtterances = [
-    '',
-    'Was ist der Kurs von {currencyTarget:Currency}',
-    'Was ist der Kurs fuer {currencyTarget:Currency}',
-    'Was ist der Kurs auf {currencyTarget:Currency}',
-    'Was ist der Wechselkurs von {currencyTarget:Currency}',
-    'Was ist der Wechselkurs fuer {currencyTarget:Currency}',
-    'Was ist der Wechselkurs auf {currencyTarget:Currency}',
-    'Wieviel ist der Kurs von {currencyTarget:Currency}',
-    'Wieviel ist der Kurs fuer {currencyTarget:Currency}',
-    'Wieviel ist der Kurs auf {currencyTarget:Currency}',
-    'Wieviel ist der Wechselkurs von {currencyTarget:Currency}',
-    'Wieviel ist der Wechselkurs fuer {currencyTarget:Currency}',
-    'Wieviel ist der Wechselkurs auf {currencyTarget:Currency}',
-    'Wie ist der Kurs von {currencyTarget:Currency}',
-    'Wie ist der Kurs fuer {currencyTarget:Currency}',
-    'Wie ist der Kurs auf {currencyTarget:Currency}',
-    'Wie ist der Wechselkurs von {currencyTarget:Currency}',
-    'Wie ist der Wechselkurs fuer {currencyTarget:Currency}',
-    'Wie ist der Wechselkurs auf {currencyTarget:Currency}',
-    'nach dem Kurs von {currencyTarget:Currency}',
-    'nach dem Kurs fuer {currencyTarget:Currency}',
-    'nach dem Kurs auf {currencyTarget:Currency}',
-    'nach dem Wechselkurs von {currencyTarget:Currency}',
-    'nach dem Wechselkurs fuer {currencyTarget:Currency}',
-    'nach dem Wechselkurs auf {currencyTarget:Currency}'
+  const bcUtterances = [
+    'nach dem Kurs',
+    'nach dem Wechselkurs',
+    'nach dem Preis',
+    'was ist der Kurs',
+    'was ist der Wechselkurs',
+    'was ist der Preis',
+    'wieviel ist der Kurs',
+    'wieviel ist der Wechselkurs',
+    'wieviel ist der Preis',
+    'wie ist der Kurs',
+    'wie ist der Wechselkurs',
+    'wie ist der Preis',
+    'los',
+    'start',
+    'nach Kursen'
   ];
 
-  app.intent('BitcoinRateIntent', eurUtterances, (slots, attrs, data, done) => {
-    if (!slots.currencyTarget) {
-      return {
-        text: 'Diese Waehrung kenne ich nicht',
-        end: false
-      };
-    } else {
-      var targetC = dictionary.currencies[slots.currencyTarget];
+  app.intent('BitcoinRateIntent', bcUtterances, (slots, attrs, data, done) => {
+    
+    var apiKey = JSON.stringify(process.env.QUANDL_API_KEY).replace(/"/g, '');
+    var date = moment().subtract(1, 'days').format('YYYY-MM-DD');
+    var url = `https://www.quandl.com/api/v3/datasets/BTCE/USDBTC.json?api_key=${apiKey}&start_date=${date}`;
 
-      axios.get(`https://api.fixer.io/latest?base=EUR&symbols=${targetC}`).then(result => {
-        if (result.data) {
-          var rate = result.data.rates[targetC];
-          rate = rate.toFixed(3);
+    console.log('debug custom: ', url);
 
-          var currencyTarget = dictionary.currencyNamesPlural[targetC];
-
-          done(`Ein Euro sind ${rate} ${currencyTarget}`);
-        } else {
-          done({
-            text: 'Entschuldige, da hat was nicht geklappt',
-            end: false
+    axios.get(url).then(result => {
+      console.log('debug custom: ', result.data);
+      if (result.data) {
+        // handle errors
+        if (!result.data.dataset.data[0] || typeof result.data.dataset.data[0] === 'undefined') {
+          done(() => {
+            return {
+              text: 'Ich konnte die Kurse nicht abrufen'
+            };
           });
         }
-      });
+        // get usd rates as strings
+        var high = '' + result.data.dataset.data[0][1].toFixed(3);
+        var low = '' + result.data.dataset.data[0][2].toFixed(3);
+        var avg = '' + result.data.dataset.data[0][3].toFixed(3);
+        var last = '' + result.data.dataset.data[0][4].toFixed(3);
+        var buy = '' + result.data.dataset.data[0][5].toFixed(3);
+        var sell = '' + result.data.dataset.data[0][6].toFixed(3);
 
-    }
+        // replace dots with commas
+        high = high.replace('.', ',');
+        low = low.replace('.', ',');
+        avg = avg.replace('.', ',');
+        last = last.replace('.', ',');
+        buy = buy.replace('.', ',');
+        sell = sell.replace('.', ',');
+
+        // build output speech
+        var speech = `<speak>`;
+        speech += `<s>Der heutige Durschschnittskurs fuer einen Bitcoin steht bei ${avg} U.S. Dollar, der aktuellste Preis ist ${last}</s>`;
+        speech += `<s>Der Kaufpreis steht bei ${buy}, der Verkaufspreis bei ${sell} Dollar</s>`;
+        speech += `<s>Der heutige Hoechststand war ${high}, der Tiefststand ${low} Dollar</s>`;
+        speech += `</speak>`;
+
+        console.log('debug custom: ', speech);
+
+        done(() => {
+          return {
+            ssml: true,
+            text: speech
+          };
+
+        });
+
+      } else {
+        done(() => {
+          return {
+            text: 'Ich konnte die Kurse nicht abrufen'
+          };
+        });
+      }
+    });
+
   });
 };
