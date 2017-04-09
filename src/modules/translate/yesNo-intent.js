898b8ae7f75483ecd9e8071b99b603b9e3309a7d
Martin Kainzbauer <m.kainzbauer@xtools.at>
2017-04-09 02:23:04 +0200

    ready for first test an AWS

--- src/modules/translate/yesNo-intent.js
+++ src/modules/translate/yesNo-intent.js
@@ -2,12 +2,20 @@
 
 module.exports = app => {
 
-  app.builtInIntent('yes', () => {
-    return {
-      ssml: true,
-      text: app.t('text'),
-      end: false
-    };
+  app.builtInIntent('yes', (slots, attrs, data) => {
+    console.log(attrs.previousIntent);
+    if (attrs.previousIntent == 'TranslateIntent' && typeof attrs.lastPhrase !== undefined && attrs.lastPhrase !== '') {
+      return {
+        ssml: true,
+        text: app.t('textSpellOut', attrs),
+        end: false
+      };
+    } else {
+      return {
+        text: '',
+        end: false
+      };
+    }
   });
 
   app.builtInIntent('no', () => {
